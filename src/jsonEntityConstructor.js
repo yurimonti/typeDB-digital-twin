0c990f78704ba081770ec5f9aebd5d66a5b2ffdf
Giacomo <giacomo.matteucci@studenti.unicam.it>
2023-03-15 11:52:07 +0100

    Deleted useless code

--- src/jsonEntityConstructor.js
+++ src/jsonEntityConstructor.js
@@ -1,25 +1,3 @@
-/* module.exports = class JsonEntityConstructor {
-    #entityType;
-    #attributes;
-    constructor(entity,attribute) {
-      this.#entityType = entity._type._label._name;
-      this.#attributes = {[attribute._type._label._name]:attribute._value};
-    }
-
-    get entityType(){
-        return this.#entityType;
-    }
-
-    get attributes(){
-        return this.#attributes;
-    }
-
-    createJson() {
-        return {[this.entityType]:this.attributes}
-    }
-  }
-
- */
 require("express");
 
 
